1b7182287d0fe60516292e68512147dc52160472
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-23 06:50:31 -0700

    test: hello world endpoint

---
+++ tests/test_endpoints.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_home_should_respond_with_hello_world_message():
+    response = client.get("/")
+    assert response.json() == {"message": "Hello World!"}
