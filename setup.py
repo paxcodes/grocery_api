89038a4c46aadb4c599a0655717b487ff866bb2f
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-24 13:09:42 -0700

    build: add setup.py so we can install our project
    ...and able to use debuggers (VS Code) and import our packages properly!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="grocery_api",
+    version="1.0",
+    description="A simple API that creates, retrieves, and updates grocery data.",
+    author="Pax",
+    author_email="groceryapi@paxmargret.33mail.com",
+    packages=["grocery_api"],
+)
