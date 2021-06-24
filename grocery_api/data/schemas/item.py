15de19b885e9e7eecd58e0ea9c0445ef772abdde
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-24 08:12:57 -0700

    feat: add attributes for schemas

--- grocery_api/data/schemas/item.py
+++ grocery_api/data/schemas/item.py
@@ -1,2 +1,12 @@
-class Item:
-    pass
+from decimal import Decimal
+from typing import Set
+
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    price: Decimal
+    is_active: bool
+    tags: Set[str]
--- grocery_api/data/schemas/store.py
+++ grocery_api/data/schemas/store.py
@@ -1,2 +1,8 @@
-class Store:
-    pass
+from pydantic import BaseModel
+
+
+class Store(BaseModel):
+    id: int
+    name: str
+    founding_year: int
+    is_active: bool
