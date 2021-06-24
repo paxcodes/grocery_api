1c2fe3a822ba8ad1adef63480243521f0da3823f
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-24 11:27:17 -0700

    test: add test for getting an item by id

--- tests/crud/test_item.py
+++ tests/crud/test_item.py
@@ -12,3 +12,17 @@ pytestmark = mark.asyncio
 
 def test_item_json_is_successfully_mocked():
     assert item.JSON_FILE == TEST_JSON_DIR / "items.json"
+
+
+async def test_get_item_by_id():
+    actual_item = await item.read(1)
+    expected_item = schemas.Item(
+        **{
+            "id": 1,
+            "name": "Salt & Pax-pper",
+            "price": 3.1,
+            "is_active": True,
+            "tags": None,
+        }
+    )
+    assert actual_item == expected_item
