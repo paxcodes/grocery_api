9b0a4823f093899be66bf640d1be5120ac9bc55f
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-24 22:12:50 -0700

    feat: get user by ID

--- grocery_api/data/crud/user.py
+++ grocery_api/data/crud/user.py
@@ -1,3 +1,7 @@
+import asyncio
+import json
+
+import aiofiles
 from pydantic import UUID4
 
 from grocery_api.data import schemas
@@ -6,7 +10,19 @@ from grocery_api.data.crud.source import JSON_DIRECTORY
 JSON_FILE = JSON_DIRECTORY / "users.json"
 
 
-async def read_user(user_id: UUID4) -> schemas.UserSensitiveData:
-    # For the sake of demonstrating FastAPI filtering data based on response_model,
-    # let's return all attributes here.
-    pass
+# Using UUID4 doesn't do runtime type-checking here, only static type-checking.
+async def read(user_id: UUID4) -> schemas.UserSensitiveData:
+    """Get a user by their ID.
+
+    Args:
+        user_id (UUID4): The user's ID.
+
+    Returns:
+        schemas.UserSensitiveData: For the sake of demonstrating FastAPI filtering data
+            based on response_model, let's return all attributes including sensitive
+            data such as their salted & hashed password and their salt.
+    """
+    async with aiofiles.open(JSON_FILE) as f:
+        file_data = await f.read()
+    json_data = json.loads(file_data)
+    return schemas.UserSensitiveData(**json_data[str(user_id)])
