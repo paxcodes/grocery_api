aa7134d01793f4ea6b996e0c65071e345eaad793
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-28 09:25:01 -0700

    fix: remove schemas in initial repo

--- grocery_api/data/schemas/__init__.py
+++ grocery_api/data/schemas/__init__.py
@@ -1,10 +1,7 @@
-from .item import ItemBase, ItemOut
 from .store import StoreBase, StoreOut
 from .user import UserSensitiveData, UserOut
 
 __all__ = [
-    "ItemBase",
-    "ItemOut",
     "StoreBase",
     "StoreOut",
     "UserSensitiveData",
