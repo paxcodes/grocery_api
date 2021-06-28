bffbc42b075809da08c3895d374513cbae0b81d0
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-28 09:25:01 -0700

    fix: remove schemas in initial repo

--- grocery_api/data/schemas/item.py
+++ grocery_api/data/schemas/item.py
@@ -1,19 +1,3 @@
-from decimal import Decimal
-from typing import Set, Optional
-
-from pydantic import BaseModel
-
-# TODO-Pre-Workshop: Strip inheritance and properties
-
-
-class ItemBase(BaseModel):
+class Item:
     # Properties: name, price, is_active, tags
-    name: str
-    price: Decimal
-    is_active: bool
-    tags: Optional[Set[str]]
-
-
-class ItemOut(ItemBase):
-    # Property: 'id'
-    id: int
+    pass
