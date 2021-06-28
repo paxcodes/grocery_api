87e32d7af684779236ba29a03f356693a4c30044
Pax <13646646+paxcodes@users.noreply.github.com>
2021-06-28 09:49:04 -0700

    refactor: remove schemas

--- grocery_api/data/schemas/__init__.py
+++ grocery_api/data/schemas/__init__.py
@@ -1,9 +1,6 @@
-from .store import StoreBase, StoreOut
 from .user import UserSensitiveData, UserOut
 
 __all__ = [
-    "StoreBase",
-    "StoreOut",
     "UserSensitiveData",
     "UserOut",
 ]
--- grocery_api/data/schemas/store.py
+++ grocery_api/data/schemas/store.py
@@ -1,17 +1,3 @@
-from typing import Optional
-from pydantic import BaseModel
-
-# TODO-Pre-Workshop: Strip inheritance and properties
-
-
-class StoreBase(BaseModel):
+class Store:
     # Properties: name, founding_year, is_active, parent_company
-    name: str
-    founding_year: int
-    is_active: bool
-    parent_company: Optional[str]
-
-
-class StoreOut(StoreBase):
-    # Property: id
-    id: int
+    pass
