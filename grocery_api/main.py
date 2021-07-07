f834a9363d2d725268a0da93429a4b6f868f077b
Pax <13646646+paxcodes@users.noreply.github.com>
2021-07-06 17:46:57 -0700

    docs: add comment for the data modules

--- grocery_api/main.py
+++ grocery_api/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
 
+# These are the "helper modules" to retrieve data. We will use these
+# inside our paths' functions.
+#
+# Example, to get all the store data:
+#   stores = await store_data.read_all()
+# To get a specific store given a store ID, store_id,
+#   store = await store_data.read(store_id)
+#
+# `item_data` and `store_data` will have very similar functions so when you
+# see me use one in `item_data` it is very likely a similar function exists
+# in `store_data`.
+#
+# You can also think of these modules as our existing "backend" or
+# "functionalities" which we will make accessible via the web using
+# the API we will be creating with the FastAPI framework.
 from grocery_api.data import item as item_data  # noqa
 from grocery_api.data import store as store_data  # noqa
 from grocery_api.data import user as user_data  # noqa
